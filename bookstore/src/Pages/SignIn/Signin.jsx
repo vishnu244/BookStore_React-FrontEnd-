d854bac5656f57a8034194a1ffe7a01d7f7e563e
vishnu244 <vishnunali244@gmail.com>
2022-09-13 19:38:31 +0530

    [Vishnu]-Added SignIn Component

--- bookstore/src/Pages/SignIn/Signin.jsx
+++ bookstore/src/Pages/SignIn/Signin.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import onlineShopping from '../../Assets/OnlineShpping.png'
 import './Signin.css'
 
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 function Signin() {
     return (
         <div>
@@ -13,20 +16,23 @@ function Signin() {
                     </div>
                     <div class="box2">
                         <div className="buttons">
-                            <button style={{ backgroundColor: "white", border:"none"}}>LOGIN</button>
-                            <button style={{ backgroundColor: "white", border:"none"}}>SIGNUP</button>
+
+                            <button style={{ backgroundColor: "white", border: "none", fontWeight:"bold" }}>LOGIN</button>
+                            <button style={{ backgroundColor: "white", border: "none", color:"grey" }}>SIGNUP</button>
                         </div>
                         <div className="Data">
                             <div className="inputdetails">
-                                
-                                <input type="email" placeholder=" Email Id " />
-                                <input type="Password" placeholder=" Password " />
-                                <button>Login</button>
+                                <label className="emailid">Email Id</label>
+                                <TextField id="outlined-basic" className="emailinput" variant="outlined" size="small" placeholder=" Email Id " style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start",height:"50px" }} />
+                                <label className="emailid">Password</label>
+                                <TextField id="outlined-basic" className="emailinput" variant="outlined" size="small" placeholder=" Password " style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start",height:"50px"  }} />
+                                <label className="forgotpass" >Forgot Password?</label>
+                                <button style={{ borderRadius: "5px", color:"white"}}>Login</button>
                                 <text>OR</text>
                             </div>
                             <div className="FBGgl" >
-                                <button style={{ backgroundColor: "#4266B2", border: "grey" }}>FaceBook</button>
-                                <button style={{ backgroundColor: "#F5F5F5", border: "grey" }}>Google</button>
+                                <button style={{ backgroundColor: "#4266B2", border: "grey", borderRadius:"5px", color:"white" }}>FaceBook</button>
+                                <button style={{ backgroundColor: "#F5F5F5", border: "grey", borderRadius:"5px" }}>Google</button>
                             </div>
 
                         </div>
