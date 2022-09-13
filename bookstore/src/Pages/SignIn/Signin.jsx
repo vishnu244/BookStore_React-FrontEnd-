c631e3847d034264b62c5670c64ce148c9299c9b
vishnu244 <vishnunali244@gmail.com>
2022-09-13 22:23:39 +0530

    [Vishnu]-Added SignIn Component

--- bookstore/src/Pages/SignIn/Signin.jsx
+++ bookstore/src/Pages/SignIn/Signin.jsx
@@ -3,7 +3,6 @@ import onlineShopping from '../../Assets/OnlineShpping.png'
 import './Signin.css'
 
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 
 function Signin() {
     return (
@@ -16,31 +15,26 @@ function Signin() {
                     </div>
                     <div class="box2">
                         <div className="buttons">
-
-                            <button style={{ backgroundColor: "white", border: "none", fontWeight:"bold" }}>LOGIN</button>
-                            <button style={{ backgroundColor: "white", border: "none", color:"grey" }}>SIGNUP</button>
+                            <button style={{ fontWeight: "bold", color: "black" }}>LOGIN</button>
+                            <button >SIGNUP</button>
                         </div>
                         <div className="Data">
                             <div className="inputdetails">
                                 <label className="emailid">Email Id</label>
-                                <TextField id="outlined-basic" className="emailinput" variant="outlined" size="small" placeholder=" Email Id " style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start",height:"50px" }} />
+                                <TextField id="outlined-basic" variant="outlined" size="small" placeholder=" Email Id " style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", height: "50px" }} />
                                 <label className="emailid">Password</label>
-                                <TextField id="outlined-basic" className="emailinput" variant="outlined" size="small" placeholder=" Password " style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start",height:"50px"  }} />
+                                <TextField id="outlined-basic" variant="outlined" size="small" placeholder=" Password " style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", height: "50px" }} />
                                 <label className="forgotpass" >Forgot Password?</label>
-                                <button style={{ borderRadius: "5px", color:"white"}}>Login</button>
+                                <button >Login</button>
                                 <text>OR</text>
                             </div>
                             <div className="FBGgl" >
-                                <button style={{ backgroundColor: "#4266B2", border: "grey", borderRadius:"5px", color:"white" }}>FaceBook</button>
-                                <button style={{ backgroundColor: "#F5F5F5", border: "grey", borderRadius:"5px" }}>Google</button>
+                                <button style={{ color: "white" }}>FaceBook</button>
+                                <button style={{ backgroundColor: "#F5F5F5" }}>Google</button>
                             </div>
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
         </div>
     )
